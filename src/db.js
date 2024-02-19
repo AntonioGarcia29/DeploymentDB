6d5c893e896cfbe463e2cf0f2edb3691d64f2439
Antonio Iván García Galván <90354712+AntonioGarcia29@users.noreply.github.com>
2024-02-18 19:03:27 -0600

    Esperando config de dns

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://mongo:27018/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://mongo.mondongo.svc.cluster.local:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(db => console.log('Mondongo is connected to ', db.connection.host))
     .catch(error => console.error('Error connecting to Mondongo:', error));
 
